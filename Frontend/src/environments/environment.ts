6f38c028c42c3e7f729e3de677d9cad239fa0c6d
Antriksh <antriksh199@gmail.com>
2026-07-12 13:58:49 +0530

    GitHub secrets correction

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -3,9 +3,9 @@
 export const configuration = {
     awsCognitoSettings: {
         domain: '',
-        userPoolId: 'COGNITO_USER_POOL_ID',
-        clientId: 'COGNITO_CLIENT_ID',
-        authority: 'COGNITO_AUTHORITY',
+        userPoolId: 'GH_COGNITO_USER_POOL_ID',
+        clientId: 'GH_COGNITO_CLIENT_ID',
+        authority: 'GH_COGNITO_AUTHORITY',
         redirectUrl: 'https://monetary.antriksh.site/',
         logoutURL:'https://monetary.antriksh.site/',
         scope: ['openid', 'phone', 'email'],
